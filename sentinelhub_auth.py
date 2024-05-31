da9b78e1b379a2237d56bae4d553e63d4f13716a
Alim Karimi <32401390+alimkarimi@users.noreply.github.com>
2024-05-30 21:27:34 -0700

    Create sentinelhub_auth.py
    getting data from sentinelhub

---
+++ sentinelhub_auth.py
@@ -0,0 +1,20 @@
+from oauthlib.oauth2 import BackendApplicationClient
+from requests_oauthlib import OAuth2Session
+
+# Your client credentials
+client_id = ''
+client_secret = ''
+
+# Create a session
+client = BackendApplicationClient(client_id=client_id)
+oauth = OAuth2Session(client=client)
+
+# Get token for the session
+token = oauth.fetch_token(token_url='https://services.sentinel-hub.com/auth/realms/main/protocol/openid-connect/token',
+                          client_secret=client_secret, include_client_id=True)
+
+access_token = token['access_token']
+
+# All requests using this session will have an access token automatically added
+resp = oauth.get("https://services.sentinel-hub.com/configuration/v1/wms/instances")
+#print(resp.content)
